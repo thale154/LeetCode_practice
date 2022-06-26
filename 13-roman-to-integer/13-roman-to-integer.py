42cf43b14e68ceab4ecbb238c7328b86c8f9ba7b
Thale <lethanhtai154@gmail.com>
2022-06-26 23:26:47 +0200

    Time: 76 ms (44.71%), Space: 14 MB (30.40%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -3,7 +3,6 @@ class Solution:
         roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         result = 0
         for i in range(len(s) - 1, -1, -1):
-            num = roman[s[i]]
-            result += num if 4 * num >= result else (-num)
+            result += roman[s[i]] if 4 * roman[s[i]] >= result else (-roman[s[i]])
         return result
         
