a4a0167296697d306b5c86eef21b7db74616a9f2
Thale <lethanhtai154@gmail.com>
2022-06-27 16:02:10 +0200

    Time: 1625 ms (8.10%), Space: 39.1 MB (76.73%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # two pointers fast - slow - Floyd cycle detection
+        fast = slow = head
+        prev = None # previous node 
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        prev, slow, prev.next = slow, slow.next, None
+        while slow:
+            #tmp = slow.next
+            slow.next, prev, slow = prev, slow, slow.next
+        fast, slow = head, prev
+        while slow:
+            if fast.val != slow.val: return False
+            fast, slow = fast.next, slow.next
+        return True
