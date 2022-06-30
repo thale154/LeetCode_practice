9647bbbf1f803622aa6d415b054f54a2ba29e276
Thale <lethanhtai154@gmail.com>
2022-06-30 18:55:51 +0200

    Time: 64 ms (100.00%), Space: 15.5 MB (33.43%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -2,5 +2,5 @@ class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         n = len(nums)
         median = sorted(nums)[n // 2]
-        
-        return sum([abs(median - num) for num in nums]) 
+        res = sum([abs(median - num) for num in nums])
+        return res
