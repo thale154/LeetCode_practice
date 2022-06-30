8b02ed8114a3c359c037c7f325c0ce2abc5cc1df
Thale <lethanhtai154@gmail.com>
2022-06-30 19:57:17 +0200

    Time: 62 ms (12.89%), Space: 13.9 MB (54.67%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -7,7 +7,7 @@ class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast = head
         slow = head
-        while (fast) and (fast.next):
+        while (fast != None) and (fast.next != None):
             fast = fast.next.next
             slow = slow.next
         return slow
