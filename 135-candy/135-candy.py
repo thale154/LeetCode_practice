8dd66fefd22fc26ea469a87905d6984f684c465b
Thale <lethanhtai154@gmail.com>
2022-07-04 10:29:34 +0200

    Time: 222 ms (61.75%), Space: 16.7 MB (74.92%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -4,10 +4,15 @@ class Solution:
         if n == 1:
             return 1
         candies = [1] * n
-        for i in range(n-1): #left to right
+        
+         #left to right
+        for i in range(n-1):
             if ratings[i] < ratings[i+1]:
                 candies[i+1] = candies[i] + 1
-        for i in range(n-1, 0, -1): #right to left
+                
+        #right to left
+        for i in range(n-1, 0, -1): 
             if ratings[i] < ratings[i-1]:
                 candies[i - 1] = max(candies[i-1], candies[i] + 1)
+                
         return sum(candies)
