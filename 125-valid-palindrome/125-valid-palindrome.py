5dabaa4bf5cf0f1d8498a8ebad0bc08f9ffa4a29
Thale <lethanhtai154@gmail.com>
2022-09-14 16:44:25 +0200

    Time: 42 ms (97.64%), Space: 15.5 MB (19.78%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         import re
         cleaned = re.sub(r'[^a-z0-9]', '', s.lower())
-        for i in range(len(cleaned)):
+        for i in range(len(cleaned)//2):
             if (cleaned[i] != cleaned[len(cleaned)-i-1]):
                 return False
         return True
