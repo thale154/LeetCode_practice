6ca4a149260208bad4a54412869199edf1911774
Thale <lethanhtai154@gmail.com>
2022-09-16 09:22:58 +0200

    Time: 246 ms (5.14%), Space: 18.8 MB (68.13%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -14,5 +14,4 @@ class Solution:
             return self.lowestCommonAncestor(root.left, p, q)
         elif (pVal > rootVal) and (qVal > rootVal):
             return self.lowestCommonAncestor(root.right, p, q)
-        else:
-            return root
+        return root
